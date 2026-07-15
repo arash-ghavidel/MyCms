16e737b3f19dc0dc7c253bfd6597a54264c28621
Arash Ghavidel <ghavidelarash@gmail.com>
2026-07-15 13:39:36 +0330

    Created Repositories and Services for PageGroup and Pages

---
+++ DataLayer/Services/PageRepository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataLayer
+{
+    public class PageRepository : IPageRepository
+    {
+        MyCmsContext db = new MyCmsContext();
+        public IEnumerable<Page> GetAllPage()
+        {
+            return db.Pages;
+        }
+
+        public Page GetPageById(int pageId)
+        {
+            return db.Pages.Find(pageId);
+        }
+
+        public bool InsertPage(Page page)
+        {
+            try
+            {
+                db.Pages.Add(page);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool UpdatePage(Page page)
+        {
+            try
+            {
+                db.Entry(page).State = EntityState.Modified;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool DeletePage(int pageId)
+        {
+            try
+            {
+                var page = GetPageById(pageId);
+                DeletePage(page);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeletePage(Page page)
+        {
+            try
+            {
+                db.Entry(page).State = EntityState.Deleted;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
