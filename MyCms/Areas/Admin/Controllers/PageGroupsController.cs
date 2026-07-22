6a4b2a4799e47455168abb8697bbaefb046e1497
Arash Ghavidel <ghavidelarash@gmail.com>
2026-07-22 13:31:08 +0330

    Modal for editing the groups added to the project

--- MyCms/Areas/Admin/Controllers/PageGroupsController.cs
+++ MyCms/Areas/Admin/Controllers/PageGroupsController.cs
@@ -75,7 +75,7 @@ namespace MyCms.Areas.Admin.Controllers
             {
                 return HttpNotFound();
             }
-            return View(pageGroup);
+            return PartialView(pageGroup);
         }
 
         // POST: Admin/PageGroups/Edit/5
