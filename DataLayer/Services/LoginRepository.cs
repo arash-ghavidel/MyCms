91a2df56e84daf2e8d4192d9fb95f3ca279827e5
Arash Ghavidel <ghavidelarash@gmail.com>
2026-09-02 13:36:18 +0330

    Repository and view model for login page created

---
+++ DataLayer/Services/LoginRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private MyCmsContext db;
+        public LoginRepository(MyCmsContext context)
+        {
+            db = context;
+        }
+        public bool IsUserValid(string username, string password)
+        {
+            return db.AdminLogins.Any(u => u.Username == username && u.Password == password);
+        }
+    }
+}
---
+++ DataLayer/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [MaxLength(200)]
+        public string Username { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [MaxLength(200)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+}
