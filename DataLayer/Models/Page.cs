4109e62450c95a28d691e1eb684aae5f926c7839
Arash Ghavidel <ghavidelarash@gmail.com>
2026-09-09 14:35:37 +0330

    CKEditor added to the project

--- DataLayer/Models/Page.cs
+++ DataLayer/Models/Page.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace DataLayer
 {
@@ -30,6 +31,7 @@ namespace DataLayer
         [Display(Name = "متن")]
         [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
         [DataType(DataType.MultilineText)]
+        [AllowHtml]
         public string Text { get; set; }
 
         [Display(Name = "بازدید")]
